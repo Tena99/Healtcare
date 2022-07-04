29e71ae6b205b880d1b338e3473cd7b5f2f21eb3
Tena99 <strelka1515@gmail.com>
2022-07-04 11:41:27 +0300

    Added header, 1 and 2 section, fonts, bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(".telephone_icon").hide(100, function () {
+  setTimeout(function () {
+    $(".telephone_icon").show(500);
+  }, 1000);
+});
+
+setTimeout(function () {
+  $(".telephone_icon").addClass("magictime twisterInUp");
+}, 1000);
+
+$(".messenger_icon").hide(100, function () {
+  setTimeout(function () {
+    $(".messenger_icon").show(500);
+  }, 1500);
+});
+
+setTimeout(function () {
+  $(".messenger_icon").addClass("magictime twisterInUp");
+}, 1500);
+
+$(".rate_icon").hide(100, function () {
+  setTimeout(function () {
+    $(".rate_icon").show(500);
+  }, 2000);
+});
+
+setTimeout(function () {
+  $(".rate_icon").addClass("magictime twisterInUp");
+}, 2000);
+
+$(".messenger_green_icon").hide(100, function () {
+  setTimeout(function () {
+    $(".messenger_green_icon").show(500);
+  }, 2500);
+});
+
+setTimeout(function () {
+  $(".messenger_green_icon").addClass("magictime twisterInDown");
+}, 2500);
+
+$(".heart_icon").hide(100, function () {
+  setTimeout(function () {
+    $(".heart_icon").show(500);
+  }, 3000);
+});
+
+setTimeout(function () {
+  $(".heart_icon").addClass("magictime twisterInDown");
+}, 3000);
